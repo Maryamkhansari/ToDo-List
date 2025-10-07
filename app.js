55207d4f50498ce8c0305b661d7d7a22cf9008b1
Maryam khansari <199159823+Maryamkhansari@users.noreply.github.com>
2025-10-07 13:49:31 +0330

    Update app.js

--- app.js
+++ app.js
@@ -134,5 +134,22 @@ themeToggle.addEventListener("click", () => {
   }
 });
 
+function updateClock() {
+  const now = new Date();
+  const options = { 
+    weekday: 'long', 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric', 
+    hour: '2-digit', 
+    minute: '2-digit', 
+    second: '2-digit' 
+  };
+  const formattedTime = now.toLocaleString('fa-IR', options);
+  document.getElementById('clock').textContent = formattedTime;
+}
 
+
+setInterval(updateClock, 1000);
+updateClock();
 
